20d44de466e327b6865fe19823b500bc5b980c12
watcharaphat <watcharaphat.3166@gmail.com>
2016-10-30 16:21:18 +0700

    rework in newHTML

---
+++ newHTML/js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+      
+      $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      });
+      
+      $(".dropdown-button").dropdown();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
