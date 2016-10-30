71c3f1c0df549715ee8a134d422ed6c5228b4a92
watcharaphat <watcharaphat.3166@gmail.com>
2016-10-30 18:00:59 +0700

    add carousel

--- newHTML/js/init.js
+++ newHTML/js/init.js
@@ -8,6 +8,9 @@
       });
       
       $(".dropdown-button").dropdown();
+      
+      $('.carousel.carousel-slider').carousel({full_width: true});
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
